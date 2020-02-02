917ef058c08e685e7bdb4e349b07c8f0c5f7a926
Oliver Diachuk <Homefolder@Olivers-MBP.hitronhub.home>
2020-02-02 09:16:32 -0800

    added sound

---
+++ Assets/jetPack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jetPack : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print(" Rotating left");
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print(" Rotating right");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
